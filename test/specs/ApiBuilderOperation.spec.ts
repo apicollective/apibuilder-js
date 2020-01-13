bab9079e20f874b50fc76cf240fb123eb2a8854b
Christian Muñoz <christian.munoz@flow.io>
2020-01-13 15:01:45 -0500

    fix compilation errors in unit tests

--- test/specs/ApiBuilderOperation.spec.ts
+++ test/specs/ApiBuilderOperation.spec.ts
@@ -1,16 +1,16 @@
 import { ApiBuilderService } from '../../src';
-import apidocApiJson from '../fixtures/apidoc-api.json';
+import apidocApi from '../fixtures/apidoc-api';
 
 describe('ApiBuilderOperation', () => {
   describe('#getResponseTypeByCode', () => {
     test('returns type matching response code', () => {
-      const service = new ApiBuilderService(apidocApiJson);
+      const service = new ApiBuilderService(apidocApi);
       const resource = service.resources
         .find(resource => resource.typeName === 'application');
-      const operation = resource.operations
+      const operation = resource?.operations
         .find(operation => operation.path === '/:orgKey' && operation.method === 'GET');
-      const type = operation.getResponseTypeByCode(200);
-      expect(type.toString()).toEqual('[com.bryzek.apidoc.api.v0.models.application]');
+      const type = operation?.getResponseTypeByCode(200);
+      expect(type?.toString()).toEqual('[com.bryzek.apidoc.api.v0.models.application]');
     });
   });
 });
